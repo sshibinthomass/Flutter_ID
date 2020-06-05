bd056e68a52af60543571f0eedb41c0b72b733de
Shibin Thomas <sshibinthomass@gmail.com>
2020-06-05 20:04:35 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/_DSC0067.jpg'),
+              ),
+              Text(
+                "Shibin Thomas",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "MACHINE LEARNING",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 300,
+                child: Divider(
+                  height: 1,
+                  thickness: 2,
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 25,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      "+91 7904050237",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "sshibinthomass@gmail.com",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
